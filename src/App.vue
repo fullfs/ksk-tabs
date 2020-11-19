8f32a5861413c7965d13eaef80a4384bcddb0a39
Andrey Solodovnikov <raiky@yandex.ru>
2020-11-19 16:45:45 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<template>
+<div id="app">
+  Hello world
+</div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
